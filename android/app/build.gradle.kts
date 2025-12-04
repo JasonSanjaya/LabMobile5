fe3cf25b858ca01c4e33765c5193258ce922a85f
Jasonsanjaya <jasonsanjaya1503@gmail.com>
2025-12-04 11:20:19 +0700

    Perbaikan error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,6 @@
 import java.util.Properties
 import java.io.FileInputStream
 
-plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 val keystorePropertiesFile = rootProject.file("key.properties")
 val keystoreProperties = Properties()
 if (keystorePropertiesFile.exists()) {
@@ -17,10 +9,20 @@ if (keystorePropertiesFile.exists()) {
     }
 }
 
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
-    namespace = "com.example.main2"
+    namespace = "com.example.anime_verse"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -31,8 +33,7 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    // FIX: Buat signingConfigs supaya "release" EXIST
-   signingConfigs {
+    signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as? String
             keyPassword = keystoreProperties["keyPassword"] as? String
@@ -43,7 +44,10 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.main2"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.anime_verse"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -52,7 +56,8 @@ android {
 
     buildTypes {
         release {
-            // Biarkan tetap mengarah ke "release"
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so flutter run --release works.
             signingConfig = signingConfigs.getByName("release")
         }
     }
